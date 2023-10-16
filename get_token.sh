69fbf3ff00671f02657cd67fc70b86c99d489be9
truefish <truefish@mail.ru>
2023-10-16 11:19:06 +0300

    Update get_token.sh

--- get_token.sh
+++ get_token.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# In browser enter to Yandex Music URL whis this command for get token of your Yandex Music account
 /opt/google/chrome/chrome --enable-logging=stderr 'https://oauth.yandex.ru/authorize?response_type=token&client_id=23cabbbdc6cd418abb4b39c32c41195d' -v=4 > tmp.txt 2>&1 &
 sleep 2
 grep -e 'access_token=' tmp.txt
